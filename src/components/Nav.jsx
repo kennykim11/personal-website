45081f9512db929068aaa95361e0561dc211a539
Kenny Kim <kennykim11@gmail.com>
2023-03-06 19:53:32 -0500

    Navbar laid out

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,9 +2,18 @@ import HeadshotImage from '../assets/Suit_Edited_cropped_scaled.png'
 
 export function Nav() {
     return <div id="nav">
-        <div id="headshotArea">
-            <img src={HeadshotImage} id="headshotImage"></img>
+        <div id="homeArea">
+            <div id="headshotArea">
+                <img src={HeadshotImage} id="headshotImage"></img>
+            </div>
+            <h2 id="name">KENNY KIM</h2>
         </div>
-        <h2 id="name">KENNY KIM</h2>
+        <div id="navLinkArea">
+            <h2 class="navLink" id="navLink_stories">STORIES</h2>
+            <h2 class="navLink" id="navLink_summary">SUMMARY</h2>
+            <h2 class="navLink" id="navLink_testim">TESTIMONIALS</h2>
+            <h2 class="navLink" id="navLink_tools">TOOLS</h2>
+            <h2 class="navLink" id="navLink_contact">CONTACT</h2>
+        </div>
     </div>
 }
